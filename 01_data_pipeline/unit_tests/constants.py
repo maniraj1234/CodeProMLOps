ce0eb30e0bbd2ab997f48d33d0f261df9165ffe4
Maniraj Madishetty <mmadishetty@microsoft.com>
2023-04-14 20:36:55 +0530

    init

---
+++ 01_data_pipeline/unit_tests/constants.py
@@ -0,0 +1,18 @@
+# You can create more variables according to your project. The following are the basic variables that have been provided to you
+BASE_PATH = 'c:\\Users\\mmadishetty\\Downloads\\Course\\Semester 3\\MLOps\\Assignment\\Assignment\\01_data_pipeline\\'
+DB_PATH =  BASE_PATH + "unit_tests\\"
+DB_FILE_NAME = 'utils_output.db'
+UNIT_TEST_DB_FILE_NAME = 'unit_test_cases.db'
+DATA_DIRECTORY = BASE_PATH + 'unit_tests\\'
+INTERACTION_MAPPING = 'interaction_mapping.csv'
+INDEX_COLUMNS_TRAINING = ['created_date', 'first_platform_c',
+       'first_utm_medium_c', 'first_utm_source_c', 'total_leads_droppped', 'city_tier',
+       'referred_lead', 'app_complete_flag']
+INDEX_COLUMNS_INFERENCE = ['created_date', 'first_platform_c',
+       'first_utm_medium_c', 'first_utm_source_c', 'total_leads_droppped', 'city_tier',
+       'referred_lead', 'app_complete_flag']
+NOT_FEATURES = ['interaction_type']
+
+
+
+
