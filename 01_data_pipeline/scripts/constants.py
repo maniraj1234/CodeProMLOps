27cc1411f8341089ad2391636015785ecf3e8a55
Maniraj Madishetty <mmadishetty@microsoft.com>
2023-04-14 22:57:18 +0530

    updating order for UT

--- 01_data_pipeline/scripts/constants.py
+++ 01_data_pipeline/scripts/constants.py
@@ -5,11 +5,11 @@ DB_FILE_NAME = 'utils_output.db'
 UNIT_TEST_DB_FILE_NAME = 'unit_test_cases.db'
 DATA_DIRECTORY = BASE_PATH + 'notebooks\\Data\\'
 INTERACTION_MAPPING = 'interaction_mapping.csv'
-INDEX_COLUMNS_TRAINING = ['created_date', 'first_platform_c',
-       'first_utm_medium_c', 'first_utm_source_c', 'total_leads_droppped', 'city_tier',
+INDEX_COLUMNS_TRAINING = ['created_date', 'city_tier', 'first_platform_c',
+       'first_utm_medium_c', 'first_utm_source_c', 'total_leads_droppped', 
        'referred_lead', 'app_complete_flag']
-INDEX_COLUMNS_INFERENCE = ['created_date', 'first_platform_c',
-       'first_utm_medium_c', 'first_utm_source_c', 'total_leads_droppped', 'city_tier',
+INDEX_COLUMNS_INFERENCE = ['created_date', 'city_tier', 'first_platform_c',
+       'first_utm_medium_c', 'first_utm_source_c', 'total_leads_droppped',
        'referred_lead', 'app_complete_flag']
 NOT_FEATURES = ['interaction_type']
 
