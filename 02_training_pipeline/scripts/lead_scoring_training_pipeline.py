3ea3740f818f7195864a9d9ef5d8cfe17dea8bb8
Maniraj Madishetty <mmadishetty@microsoft.com>
2023-04-16 18:39:26 +0530

    training pipeline

--- 02_training_pipeline/scripts/lead_scoring_training_pipeline.py
+++ 02_training_pipeline/scripts/lead_scoring_training_pipeline.py
@@ -6,7 +6,16 @@ from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 
 from datetime import datetime, timedelta
-
+import sys
+import os
+sys.path.insert(0,os.path.abspath(os.path.dirname(__file__)))
+import utils
+import constants
+import importlib
+importlib.reload(constants)
+importlib.reload(utils)
+import warnings
+warnings.filterwarnings("ignore")
 
 ###############################################################################
 # Define default arguments and DAG
@@ -30,13 +39,29 @@ ML_training_dag = DAG(
 ###############################################################################
 # Create a task for encode_features() function with task_id 'encoding_categorical_variables'
 # ##############################################################################
+def encode_features_operation():
+    utils.encode_features(db_file_name=constants.DB_FILE_NAME, db_path=constants.DB_PATH, ONE_HOT_ENCODED_FEATURES = constants.ONE_HOT_ENCODED_FEATURES, FEATURES_TO_ENCODE = constants.FEATURES_TO_ENCODE)
+
+encode_features_task = PythonOperator(
+    task_id='encoding_categorical_variables',
+    provide_context=True,
+    python_callable=encode_features_operation,
+    dag=ML_training_dag)
 
 ###############################################################################
 # Create a task for get_trained_model() function with task_id 'training_model'
 # ##############################################################################
+def get_trained_model_operation():
+    utils.get_trained_model(db_file_name=constants.DB_FILE_NAME, db_path=constants.DB_PATH)
+
+get_trained_model_task = PythonOperator(
+    task_id='training_model',
+    provide_context=True,
+    python_callable=get_trained_model_operation,
+    dag=ML_training_dag)
 
 
 ###############################################################################
 # Define relations between tasks
 # ##############################################################################
-
+encode_features_task >> get_trained_model_task
--- 02_training_pipeline/scripts/utils.py
+++ 02_training_pipeline/scripts/utils.py
@@ -14,6 +14,7 @@ import numpy as np
 
 import sqlite3
 from sqlite3 import Error
+import os
 
 import mlflow
 import mlflow.sklearn
@@ -23,15 +24,37 @@ from sklearn.metrics import roc_auc_score
 import lightgbm as lgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from pycaret.classification import *
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import StratifiedKFold
 
-from Lead_scoring_training_pipeline.constants import *
+import constants
+import warnings
+warnings.filterwarnings("ignore")
 
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+import lightgbm as lgb
+from datetime import datetime
+from datetime import date
+import time
+from skopt import BayesSearchCV # run pip install scikit-optimize
+
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score, recall_score, auc
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import f1_score
+
+from lead_scoring_training_pipeline import constants
 
 ###############################################################################
 # Define the function to encode features
 # ##############################################################################
 
-def encode_features():
+def encode_features(db_file_name, db_path, ONE_HOT_ENCODED_FEATURES, FEATURES_TO_ENCODE):
     '''
     This function one hot encodes the categorical features present in our  
     training dataset. This encoding is needed for feeding categorical data 
@@ -55,13 +78,33 @@ def encode_features():
     **NOTE : You can modify the encode_featues function used in heart disease's inference
         pipeline from the pre-requisite module for this.
     '''
+    filename = os.path.join(db_path, db_file_name)
+
+    try:
+        conn = sqlite3.connect(filename)
+        df = pd.read_sql_query('SELECT * FROM model_input', conn)
+
+        # encoding
+        #df[FEATURES_TO_ENCODE] = df[FEATURES_TO_ENCODE])
+        # Next, create one-hot-encoded variables, add to dataframe, drop old columns
+        df_dummies = pd.get_dummies(df[FEATURES_TO_ENCODE], drop_first=True)
+        df = pd.concat([df, df_dummies], axis=1)
+        df.drop(FEATURES_TO_ENCODE, axis=1, inplace = True)
+
+        df['app_complete_flag'].to_sql('target', conn, if_exists='replace', index=False)
+        df[ONE_HOT_ENCODED_FEATURES].to_sql('features', conn, if_exists='replace', index=False)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
 
 
 ###############################################################################
 # Define the function to train the model
 # ##############################################################################
 
-def get_trained_model():
+def get_trained_model(db_file_name, db_path):
     '''
     This function setups mlflow experiment to track the run of the training pipeline. It 
     also trains the model based on the features created in the previous function and 
@@ -83,5 +126,103 @@ def get_trained_model():
     SAMPLE USAGE
         get_trained_model()
     '''
+    filename = os.path.join(db_path, db_file_name)
 
+    try:
+        conn = sqlite3.connect(filename)
+        y = pd.read_sql_query('SELECT * FROM target', conn)
+        X = pd.read_sql_query('SELECT * FROM features', conn)
+        
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+
+        mlflow.set_tracking_uri(constants.TRACKING_URI)
+
+        #Model Training
+                 #Model Training
+        gkf = StratifiedKFold(n_splits=10, shuffle=True, random_state=42).split(X, y) # startifyKFold 
+
+        gridParams = {
+            'learning_rate': [0.005, 0.01,0.1],
+            'n_estimators': [8,16,24,50],
+            'num_leaves': [6,8,12,16], # large num_leaves helps improve accuracy but might lead to over-fitting
+            'boosting_type' : ['gbdt', 'dart'], # for better accuracy -> try dart
+            'objective' : ['binary'],
+            'max_bin':[255, 510], # large max_bin helps improve accuracy but might slow down training progress
+            'random_state' : [500],
+            'colsample_bytree' : [0.64, 0.65, 0.66],
+            'subsample' : [0.7,0.75],
+            'reg_alpha' : [1,1.2],
+            'reg_lambda' : [1,1.2,1.4],
+            'max_depth': [1,3,5]
+            }
+
+        model_params = {
+            'objective':'binary', 
+            'num_boost_round':200, 
+            'metric':'f1',
+            'categorical_feature':[],
+            'verbose':-1,
+            'force_row_wise':True
+                       }
+
+        lgb_estimator = lgb.LGBMClassifier()
+        lgb_estimator.set_params(**model_params)
+
+        gsearch = BayesSearchCV(estimator=lgb_estimator, search_spaces=gridParams, cv=gkf,n_iter=32,random_state=0,n_jobs=-1,verbose=-1,scoring='roc_auc')
+        lgb_model = gsearch.fit(X, y)
+        best_model = lgb_model.best_estimator_
+        auc_score = lgb_model.best_score_
+        for p in gridParams:
+            print(f"Best {p} : {best_model.get_params()[p]}")
+
+
+        timestamp = str(int(time.time()))
+        with mlflow.start_run(run_name=f"LGBM_Bayes_Search_{timestamp}") as run:
+            y_pred = best_model.predict(X_test)
+
+            # Log model
+            mlflow.sklearn.log_model(best_model,registered_model_name='LightGBM',artifact_path='models')
+            # mlflow.mlflow_log_artifact(best_model, artifact_path ="sqlite:///database/mlflow_v01.db")
+
+
+            # Log params
+            model_params = best_model.get_params()
+            [mlflow.log_param(p, model_params[p]) for p in gridParams]
+
+            #Log metrics
+            acc=accuracy_score(y_pred, y_test)
+            conf_mat = confusion_matrix(y_pred, y_test)
+            precision = precision_score(y_pred, y_test,average= 'macro')
+            recall = recall_score(y_pred, y_test, average= 'macro')
+            auc = roc_auc_score(y_pred, y_test, average='macro')
+            #f1_score = f1_score(y_pred, y_test, average='macro')
+            cm = confusion_matrix(y_test, y_pred)
+            tn = cm[0][0]
+            fn = cm[1][0]
+            tp = cm[1][1]
+            fp = cm[0][1]
+            class_zero = precision_recall_fscore_support(y_test, y_pred, average='binary',pos_label=0)
+            class_one = precision_recall_fscore_support(y_test, y_pred, average='binary',pos_label=1)
+
+            mlflow.log_metric('test_accuracy', acc)
+            mlflow.log_metric("Precision", precision)
+            mlflow.log_metric("Recall", recall)
+            mlflow.log_metric("Precision_0", class_zero[0])
+            mlflow.log_metric("Precision_1", class_one[0])
+            mlflow.log_metric("Recall_0", class_zero[1])
+            mlflow.log_metric("Recall_1", class_one[1])
+            mlflow.log_metric("f1_0", class_zero[2])
+            mlflow.log_metric("f1_1", class_one[2])
+            mlflow.log_metric("False Negative", fn)
+            mlflow.log_metric("True Negative", tn)
+            #mlflow.log_metric("f1", f1_score)
+            mlflow.log_metric("auc", auc)
+
+            runID = run.info.run_uuid
+            print("Inside MLflow Run with id {}".format(runID))
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
    
